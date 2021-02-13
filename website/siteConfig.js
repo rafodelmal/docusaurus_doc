788ba80e60836437234a6148f35f79d4a7e0fee5
Rafo <rafo.reloaded@gmail.com>
2021-02-13 11:14:15 -0500

    FIX

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,7 +21,7 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
+  title: 'Test Site Rafo', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://your-docusaurus-test-site.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Soluciones ARK`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
